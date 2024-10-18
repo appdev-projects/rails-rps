c307b2fd41ff17465f2965c5a7fda046ca578ff3
Ben Purinton <ben.purinton@gmail.com>
2024-10-18 15:01:27 -0700

    wrap initializer

--- config/initializers/dev_toolbar.rb
+++ config/initializers/dev_toolbar.rb
@@ -1,8 +1,9 @@
-DevToolbar.configure do |config|
-  config.links = [
-    { name: "Routes", path: "/rails/info/routes" },
-    { name: "Database", path: "/rails/db" }, # rails_db gem must be installed
-    { name: "Data Model", path: "/erd.png" }, # erd.png must be in public/ folder
-    # etc.
-  ]
+if Rails.env.development?
+  DevToolbar.configure do |config|
+    config.links = [
+      { name: "Routes", path: "/rails/info/routes" },
+      { name: "Database", path: "/rails/db" },
+      { name: "Data Model", path: "/erd.png" }
+    ]
+  end
 end
